74fda01ec096dc0cd9ba443d5594ca8b016bb881
Lucas Ferreira Da Silva <needslucas944@gmail.com>
2025-10-25 21:34:37 -0300

    check se valor mínimo menor que valor máximo e se quantidade menor que intervalo entre número mínimo e número máximo

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+const checkValorMinimoMenorQueValorMaximo = (minNumero, maxNumero) => {
+    if (minNumero >= maxNumero) {
+        alert('O valor mínimo deve ser menor do que o valor máximo')
+        exit();
+    }    
+}
+
+const checkQuantidadeMenorQueValorDoIntervalo = (quantidade, intervalo) => {
+    if (quantidade > intervalo) {
+        alert(`Quantidade não deve ser maior do que o intervalo entre número máximo e mínimo. \nIntervalo: ${intervalo}`);
+        exit();
+    }
+}
+
 const desativarBotaoReiniciar = () => {
     let botaoReiniciar = document.getElementById("btn-reiniciar");
 
@@ -18,17 +32,23 @@ const ativarBotaoReiniciar = () => {
 
 const obterListaDeNumerosAleatorios = (quantidade, minNumero, maxNumero) => {
     let listaDeNumerosAleatorios = [];
-    let numeroSorteado = 0;
+    let numeroSorteado = -1;
+
     for (let i = 0; i < quantidade; i++) {
+        numeroSorteado = gerarNumeroAleatorio(minNumero, maxNumero);
         while (listaDeNumerosAleatorios.includes(numeroSorteado)) {
             numeroSorteado = gerarNumeroAleatorio(minNumero, maxNumero);
         }
         listaDeNumerosAleatorios.push(numeroSorteado);
     }
+
+    console.log(listaDeNumerosAleatorios);
     return listaDeNumerosAleatorios;
 }
 
 const gerarNumeroAleatorio = (minNumero, maxNumero) => {
+    minNumero = Math.ceil(minNumero);
+    maxNumero = Math.floor(maxNumero);
     return Math.floor(Math.random() * (maxNumero - minNumero + 1)) + minNumero;
 }
 
@@ -38,6 +58,11 @@ const sortear = () => {
     let minNumero = parseInt(document.getElementById("de").value);
     let maxNumero = parseInt(document.getElementById("ate").value);
     let resultado = document.getElementById("resultado");
+    let intervalo = (maxNumero - minNumero + 1);
+
+
+    checkValorMinimoMenorQueValorMaximo(minNumero, maxNumero);
+    checkQuantidadeMenorQueValorDoIntervalo(quantidade,  intervalo);
     
     let numerosSorteados = obterListaDeNumerosAleatorios(quantidade, minNumero, maxNumero);
     let labelResultado = `<label class="texto__paragrafo">Números sorteados: ${numerosSorteados}</label>`;
